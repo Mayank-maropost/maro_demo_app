da99c39bc62c5d753a11a4c1dfa16db185c5c4fe
Mayank Srivastava <mayank@maropost.com>
2017-06-02 13:21:23 +0000

    Devise task complted

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => 'dashboard#new'
+  devise_for :users 
+  resources :dashboard, only: [:new] 
 end
